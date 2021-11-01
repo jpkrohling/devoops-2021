e66b11b789fad86899689ff89edd89635ec2d4d7
Juraci Paixão Kröhling <juraci@kroehling.de>
2021-11-01 17:44:38 +0100

    Added processor logic
    Signed-off-by: Juraci Paixão Kröhling <juraci@kroehling.de>

--- devoopsprocessor/processor.go
+++ devoopsprocessor/processor.go
@@ -10,5 +10,17 @@ import (
 var processorCapabilities = consumer.Capabilities{MutatesData: true}
 
 func processTraces(ctx context.Context, td pdata.Traces) (pdata.Traces, error) {
+	rss := td.ResourceSpans()
+	for i := 0; i < rss.Len(); i++ {
+		ilss := rss.At(i).InstrumentationLibrarySpans()
+		for j := 0; j < ilss.Len(); j++ {
+			ils := ilss.At(j)
+			spans := ils.Spans()
+			for k := 0; k < spans.Len(); k++ {
+				span := spans.At(k)
+				span.Attributes().InsertString("seen.at", "devoops")
+			}
+		}
+	}
 	return td, nil
 }
